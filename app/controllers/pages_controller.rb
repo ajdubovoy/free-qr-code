28c88d065a57107c8efb8f594a85aeab80d2592b
Alexander Dubovoy <ajdubovoy@gmail.com>
2022-03-03 16:03:50 +0100

    code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def download
+    send_data RQRCode::QRCode.new(params[:content].to_s).as_png(size: 300), type: 'image/png', disposition: 'attachment'
+  end
 end
